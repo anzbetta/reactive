2b15f2a41e3f31f90c360840aa544ba26c5a5085
anzbetta <anzbetta2004@gmail.com>
2025-11-14 22:42:38 +0200

    init

---
+++ src/main/java/ua/com/reactive/gr_403_reactive/entity/Client.java
@@ -0,0 +1,19 @@
+package ua.com.reactive.gr_403_reactive.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Client {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+}
---
+++ src/main/java/ua/com/reactive/gr_403_reactive/entity/Greeting.java
@@ -0,0 +1,13 @@
+package ua.com.reactive.gr_403_reactive.entity;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+    private String message;
+}
+
